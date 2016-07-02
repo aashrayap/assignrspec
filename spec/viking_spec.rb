a773305212a6fdf63bea34f72e45f6878eee01ef
aashraya <aashraya_p@hotmail.com>
2016-07-02 17:03:31 -0500

    finished most

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -2,6 +2,7 @@ require 'buddy'
 
 describe Viking do
 	let(:v){Viking.new}
+	let(:axe){Axe.new}
 
 	describe '#initialize' do
 		it 'sets a name attribute' do
@@ -14,7 +15,26 @@ describe Viking do
 		it 'creates nil weapon by default' do
 		  expect(v.weapon).to eq(nil)
 		end
-		it  'picking up sets weapon'
-		  expect(v.pick_up_weapon)
+	end
+
+	describe '#pick_up_weapon' do
+		it  'picking up sets weapon' do
+		  v.pick_up_weapon(axe)
+		  expect(v.weapon).to_not be_nil
+		end
+	end
+
+	describe '#drop_weapon' do
+		it 'drops weapon' do
+		  v.drop_weapon
+		  expect(v.weapon).to be_nil
+		end
+	end
+
+	describe '#receive_attack' do
+		it 'causes viking to take specified damage dealt' do
+		  allow(v.receive_attack(20)).to receive(:take_damage).and_return(50)
+		  expect(v.health).to eq(80)
+		end
 	end
 end
