9f9a198bacebb783659f90304e3b83734a8149b5
aashraya <aashraya_p@hotmail.com>
2016-07-02 18:55:30 -0500

    added alot

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -2,7 +2,9 @@ require 'buddy'
 
 describe Viking do
 	let(:v){Viking.new}
+	let(:hotshot){Viking.new}
 	let(:axe){Axe.new}
+	let(:bow){Bow.new}
 
 	describe '#initialize' do
 		it 'sets a name attribute' do
@@ -11,7 +13,10 @@ describe Viking do
 		it 'sets a health attribute' do
 		  expect(Viking.new('buddy',20).health).to eq(20)
 		end
-		# it 'doesnt allow health to be changed'
+		 it 'doesnt allow health to be changed' do
+		   expect{v.health=20}.to raise_error(NoMethodError)
+		end
+
 		it 'creates nil weapon by default' do
 		  expect(v.weapon).to eq(nil)
 		end
@@ -22,6 +27,18 @@ describe Viking do
 		  v.pick_up_weapon(axe)
 		  expect(v.weapon).to_not be_nil
 		end
+
+		it 'picking up non weapon raises exception' do
+		  expect do
+		  	v.pick_up_weapon(Viking.new)
+		  end.to raise_exception
+		end
+
+		it 'replaces existing weapon' do
+			v.pick_up_weapon(axe)
+			v.pick_up_weapon(bow)
+			expect(v.weapon).to eq(bow)
+		end
 	end
 
 	describe '#drop_weapon' do
@@ -31,10 +48,32 @@ describe Viking do
 		end
 	end
 
-	describe '#receive_attack' do
+	describe '#take_damage' do
 		it 'causes viking to take specified damage dealt' do
-		  allow(v.receive_attack(20)).to receive(:take_damage).and_return(50)
+		  v.receive_attack(20)
 		  expect(v.health).to eq(80)
 		end
+		it 'take damage method is called' do
+		  expect(v).to receive(:take_damage)
+		  v.receive_attack(20)
+		end
+		it 'causes vikings health to drop' do
+		  initial_health=hotshot.health
+		  v.attack(hotshot)
+		  expect(hotshot.health).to be < initial_health
+		end
+		it 'calls attacked vikings take_damage method' do
+		  expect(hotshot).to receive(:take_damage)
+		  v.attack(hotshot)
+		end
+		it 'attacking with no weapon causes fist use' do
+		  expect(hotshot).to receive(:damage_with_fists)
+		  hotshot.attack(v)
+		end
+
 	end
+
 end
+
+
+
